fb9afd3bd6865d6f312a65fdc927e8e0c60092c0
jair <jairdeyner730791@gmail.com>
2022-10-20 14:43:04 -0500

    Final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'grupo04-d17c2',
+    appId: '1:833029544662:web:b26dc538b00cf87fa9c777',
+    storageBucket: 'grupo04-d17c2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCiZZbSnvg-1fd8qPM_iPmSVkNqEwUTK9I',
+    authDomain: 'grupo04-d17c2.firebaseapp.com',
+    messagingSenderId: '833029544662',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'grupo04-d17c2',
+    appId: '1:833029544662:web:b26dc538b00cf87fa9c777',
+    storageBucket: 'grupo04-d17c2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCiZZbSnvg-1fd8qPM_iPmSVkNqEwUTK9I',
+    authDomain: 'grupo04-d17c2.firebaseapp.com',
+    messagingSenderId: '833029544662',
+  },
   production: false
 };
 
